68b25a83a4d00bf78a6e5586464df61d766eee91
lulifa <814570123@qq.com>
2025-11-12 15:09:23 +0800

    feat: 删除掉application configuration 相关自定义service

--- aspnet-core/services/RuichenShuxin.AbpPro.Application.Contracts/GlobalUsings.cs
+++ aspnet-core/services/RuichenShuxin.AbpPro.Application.Contracts/GlobalUsings.cs
@@ -6,6 +6,7 @@ global using Volo.Abp.Account;
 global using Volo.Abp.Application.Dtos;
 global using Volo.Abp.Application.Services;
 global using Volo.Abp.AspNetCore.Mvc.ApplicationConfigurations;
+global using Volo.Abp.AspNetCore.Mvc.MultiTenancy;
 global using Volo.Abp.Authorization.Permissions;
 global using Volo.Abp.FeatureManagement;
 global using Volo.Abp.Identity;
--- aspnet-core/services/RuichenShuxin.AbpPro.Application/GlobalUsings.cs
+++ aspnet-core/services/RuichenShuxin.AbpPro.Application/GlobalUsings.cs
@@ -12,6 +12,7 @@ global using System.Threading.Tasks;
 global using Volo.Abp.Account;
 global using Volo.Abp.Application.Dtos;
 global using Volo.Abp.AspNetCore.Mvc.ApplicationConfigurations;
+global using Volo.Abp.AspNetCore.Mvc.MultiTenancy;
 global using Volo.Abp.AutoMapper;
 global using Volo.Abp.Domain.Repositories;
 global using Volo.Abp.FeatureManagement;
